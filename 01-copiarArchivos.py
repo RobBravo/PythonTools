2050fdae5d90f27ff05f5bb3181a7a354958cb58
Roberto Bravo <moiso32@gmail.com>
2023-03-27 11:15:39 -0600

    Update 01-copiarArchivos.py
    Se agrega calculo dinámico de la fecha proceso

--- 01-copiarArchivos.py
+++ 01-copiarArchivos.py
@@ -1,16 +1,26 @@
 import os
 import shutil
+from datetime import date
+from datetime import timedelta
 
 print("COPIADOR DE ARCHIVOS - BLACKZERO")
 print("--------------------------------")
 
-fecha='FECHA DEL ARCHIVO' # Mes en curso 
+# Mes en curso 
+
+dias = 1
+hoy = date.today()
+filedate = str(hoy - timedelta(days=dias))
+anio = filedate[:4]
+mes = filedate[5:7]
+dia = filedate[8:10]
+fecha = anio + mes 
 
 # Ruta de la carpeta de origen
-origen = "CARPETA DONDE SE ENCUENTRA EL ARCHIVO" + "\\" + fecha
+origen = r" " + "\\" + fecha
 
 # Ruta de la carpeta de destino
-destino = "RUTA DE LA CARPETA DESTINO" + "\\" + fecha
+destino = r" " + "\\" + fecha
 
 print("A continuacion copiaremos los archivos de la carpeta: ")
 print(origen)
@@ -21,12 +31,10 @@ print(destino)
 if not os.path.exists(destino):
     os.makedirs(destino)
 
-PREFIX="PREFIJO_DEL_ARCHIVO"
-
 # Recorremos la carpeta de origen
 for archivo in os.listdir(origen):
-    # Si el archivo empieza con cualquier patron ingresado en la variable prefijo
-    if archivo.startswith(PREFIX):
+    # Si el archivo empieza con "LISTADO_MOVIMIENTOS_"
+    if archivo.startswith("LISTADO_MOVIMIENTOS_"):
         # Creamos la ruta completa del archivo de origen
         ruta_origen = os.path.join(origen, archivo)
         # Creamos la ruta completa del archivo de destino
